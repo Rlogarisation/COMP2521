0944ff76c0e077d8934f39843fa333d16ce5faa7
Zheng Luo <z5206267@ad.unsw.edu.au>
2021-04-17 00:36:40 +1000

    Implemented basic structure for GirvanNewman algo.

--- Ass2/GirvanNewman.c
+++ Ass2/GirvanNewman.c
@@ -9,6 +9,7 @@
 #include "GirvanNewman.h"
 #include "Graph.h"
 
+
 /**
  * Generates  a Dendrogram for the given graph g using the Girvan-Newman
  * algorithm.
@@ -16,15 +17,45 @@
  * The function returns a 'Dendrogram' structure.
  */
 Dendrogram GirvanNewman(Graph g) {
-	// TODO: Implement this function
-	return NULL;
+	// Initiate a memory for pointer Dendrogram d.
+	Dendrogram d = malloc(sizeof(DNode));
+
+	
+	// 4. Repeat Steps 2 and 3 until no edges remain.
+	while (GraphNumVertices(g) != 0) {
+		// 1. Calculate the edge betweenness of all edges in the network.
+		// 3. Recalculate the edge betweenness 
+		// of all edges affected by the removal.
+		EdgeValues evs = edgeBetweennessCentrality(g);
+
+		// 2. Remove the edge(s) with the highest edge betweenness.
+		// Find the highest edge betweenness first.
+		double max = 0;
+		int src = -1, dest = -1;
+		for (int i = 0; i < evs.numNodes; i++) {
+			for (int j = 0; j < evs.numNodes; j++) {
+				if (evs.values[i][j] > max) {
+					max = evs.values[i][j];
+					src = i;
+					dest = j;
+				}
+			}
+		}
+		// Remove it.
+		GraphRemoveEdge(g, src, dest);
+	}
+
+	
+
+	return d;
 }
+
 
 /**
  * Frees all memory associated with the given Dendrogram  structure.  We
  * will call this function during testing, so you must implement it.
  */
 void freeDendrogram(Dendrogram d) {
-	// TODO: Implement this function
+	free(d);
 }
 
