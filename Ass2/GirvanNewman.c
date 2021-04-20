9d7019c8ff19fe45b1f1a02e57ff0a3faa00bf17
Zheng Luo <z5206267@ad.unsw.edu.au>
2021-04-20 15:35:06 +1000

    Initial attempt to solve part 3

--- Ass2/GirvanNewman.c
+++ Ass2/GirvanNewman.c
@@ -9,7 +9,9 @@
 #include "GirvanNewman.h"
 #include "Graph.h"
 
-
+static void dfsSearch(Graph g, Vertex *componentOf, Vertex v, int componentId);
+static Dendrogram insertDendrogram(Graph g, Dendrogram d, Vertex *componentOf, 
+int i, int numOfComponent);
 /**
  * Generates  a Dendrogram for the given graph g using the Girvan-Newman
  * algorithm.
@@ -43,12 +45,65 @@ Dendrogram GirvanNewman(Graph g) {
 		}
 		// Remove it.
 		GraphRemoveEdge(g, src, dest);
+
+		// Algorithm to assign vertices to connected component.
+		// componentOf[v] = 1, v is vertex, and 1 means first component.
+
+		// Initiate a array of vertex to store data.
+		Vertex *componentOf = malloc(evs.numNodes * sizeof(Vertex));
+		
+		for (int v = 0; v < evs.numNodes; v++) {
+			componentOf[v] = -1;
+		}
+		int componentId = 0;
+		for (int i = 0; i < evs.numNodes; i++) {
+			if (componentOf[i] == -1) {
+				dfsSearch(g, componentOf, i, componentId);
+				componentId++;
+			}
+		}
+
+		// Now the *componentOf are completed, 
+		// We put it into the Dendrogram.
+		for (int i = 0; i < evs.numNodes; i++) {
+			for (int numOfComponent = 0; numOfComponent < componentId; numOfComponent++) {
+				d = insertDendrogram(g, d, componentOf, i, numOfComponent);
+			}
+		}
 	}
 
-	
 
 	return d;
 }
+
+static void dfsSearch(Graph g, Vertex *componentOf, Vertex v, int componentId) {
+	componentOf[v] = componentId;
+	AdjList listOfOutgoing = GraphOutIncident(g, v);
+	while (listOfOutgoing != NULL) {
+		if (componentOf[listOfOutgoing->v] == -1) {
+			dfsSearch(g, componentOf, listOfOutgoing->v, 
+			componentId);
+		}
+		listOfOutgoing = listOfOutgoing->next;
+	}
+}
+
+static Dendrogram insertDendrogram(Graph g, Dendrogram d, Vertex *componentOf, 
+int i, int numOfComponent){
+
+	if (componentOf[i] == numOfComponent) {
+		d->vertex = i;
+		if (d->left == NULL) {
+			insertDendrogram(g, d->left, i);
+		}
+		else if (d->right == NULL) {
+			insertDendrogram(g, d->right, i);
+		}
+	}
+
+	return d;
+}
+
 
 
 /**
